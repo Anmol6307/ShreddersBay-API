6350c6226a537d544702d9a4dd26d12990ada3b5
Anmol Sahu <anmolsahu04012001@gmail.com>
2021-11-17 16:33:04 +0530

    Basic changes

--- API/address_api.php
+++ API/address_api.php
@@ -1,6 +1,7 @@
 <?php
 
 //http://localhost:8000/SHREDDERSBAY_API/API/user_api.php?action=select
+session_start();
 include("../DB.php");
 $form_data = json_decode(file_get_contents("php://input"));
 $data = array();
@@ -8,17 +9,22 @@ $error = array();
 
 
 
-if($_REQUEST['action']=='insert'){		
-			 	$user_id = mysqli_real_escape_string($con, $form_data->user_id);
-			 	$country_id = mysqli_real_escape_string($con, $form_data->country_id);
-			 	$state_id = mysqli_real_escape_string($con, $form_data->state_id);
-			    $city_id = mysqli_real_escape_string($con, $form_data->city_id);
-			 	$area_id = mysqli_real_escape_string($con, $form_data->area_id);
-			 	$address = mysqli_real_escape_string($con, $form_data->address);
-			 	$pin_code = mysqli_real_escape_string($con, $form_data->pin_code);
-			 	$add_type = mysqli_real_escape_string($con, $form_data->add_type);
-			    $landmark = mysqli_real_escape_string($con, $form_data->landmark);
-				$query = "INSERT INTO tbl_address(user_id, country_id, state_id, city_id, area_id,address, pin_code, add_type, landmark, status) VALUES ('$user_id', '$country_id', '$state_id', '$city_id', '$area_id', '$address', '$pin_code', '$add_type', '$landmark', '1')";
+if($_REQUEST['action']=='insert'){	
+$hidden_id =$_POST['id'];
+$user_id = $_SESSION['user_id'];
+			 	$country_id = $_POST['country'];
+			 	$state_id = $_POST['state'];
+			    $city_id = $_POST['city'];
+			 	// $area_id = $_POST['area_id'];
+			 	$address = $_POST['address'];
+			 	$pin_code = $_POST['pincode'];
+			 	// $add_type = $_POST['add_type'];
+			    $landmark = $_POST['landmark'];
+if($hidden_id==''){
+			 	
+				$query = "INSERT INTO tbl_address(user_id, country_id, state_id, city_id, address, pin_code, landmark, status) VALUES ('$user_id', '$country_id', '$state_id', '$city_id', '$address', '$pin_code', '$landmark', '1')";
+				
+			
 				if(mysqli_query($con, $query))
 				{
 				  $data["message"] = "Data Inserted";         
@@ -27,69 +33,62 @@ if($_REQUEST['action']=='insert'){
 				echo json_encode($data);
 }
 
+else
+{
+$query1 = "UPDATE tbl_address SET  user_id = '$user_id', country_id = '$country_id', state_id = '$state_id', city_id = '$city_id', address = '$address', pin_code = '$pin_code', landmark = '$landmark' WHERE addr_id='".$hidden_id."'";
+	echo $query1;
+	if(mysqli_query($con, $query1))
+	{
+		$data["message"] = "Data Updated";         
+	}
+echo json_encode($data);
+}
+}
+
 if($_REQUEST['action']=='select')
 {
-	 $query = "SELECT * FROM tbl_address";
+	 $query = "SELECT * FROM tbl_address as addr left join tbl_city as city on city.city_id=addr.city_id left join tbl_state as state on state.state_id=city.state_id left join tbl_country as country on country.country_id=state.country_id left join tbl_users as user on user.id=addr.user_id where user.id='".$_SESSION['user_id']."'";
+	 //echo "$query";
+
 	 $res = mysqli_query($con, $query);
 	 while($row = mysqli_fetch_assoc($res)){
-	 	$data = $row;
-	  // $data['name'] = $row['name'];
-	  // $data['email'] = $row['email'];
-	  // $data['mobile'] = $row['mobile'];
-	  // $data['date'] = $row['date'];
-	   echo json_encode($data);
+	 	$data[] = $row;
+	  
  	}
-
+     echo json_encode($data);
 }
 
 
 
 if($_REQUEST['action']=='select_id')
 {
-	 $query = "SELECT * FROM tbl_address WHERE addr_id='".$form_data->id."'";
-	 echo "$query";
+	 $query = "SELECT * FROM tbl_address WHERE addr_id='".$_GET['id']."'";
 	 $res = mysqli_query($con, $query);
-	 $row = mysqli_fetch_array($res);
-	 $user_id = $row['user_id'];
-	 $country_id = $row['country_id'];
-	 $state_id = $row['state_id'];
-	 $city_id = $row['city_id'];
-	 $area_id = $row['area_id'];
-	 $address = $row['address'];
-	 $pin_code = $row['pin_code'];
-	 $add_type = $row['add_type'];
-	 $landmark = $row['landmark'];
- 	 $data['status'] = $row['status'];
- 	 $data['date'] = $row['date'];
-     echo json_encode($data);
+	 $row = mysqli_fetch_assoc($res);
+	 $data = $row;
+    echo json_encode($data); 
 }
 
 
-if($_REQUEST['action']=='edit')
-{
+// if($_REQUEST['action']=='edit')
+// {
 
-	$user_id = mysqli_real_escape_string($con, $form_data->user_id);
-	$country_id = mysqli_real_escape_string($con, $form_data->country_id);
-	$state_id = mysqli_real_escape_string($con, $form_data->state_id);
-	$city_id = mysqli_real_escape_string($con, $form_data->city_id);
-	$area_id = mysqli_real_escape_string($con, $form_data->area_id);
-	$address = mysqli_real_escape_string($con, $form_data->address);
-	$pin_code = mysqli_real_escape_string($con, $form_data->pin_code);
-	$add_type = mysqli_real_escape_string($con, $form_data->add_type);
-	$landmark = mysqli_real_escape_string($con, $form_data->landmark);
-	$query = "UPDATE tbl_address SET  user_id = '$user_id', country_id = '$country_id', state_id = '$state_id', city_id = '$city_id',area_id = '$area_id', address = '$address', pin_code = '$pin_code', add_type = '$add_type', landmark = '$landmark' WHERE addr_id='".$form_data->id."'";
-	echo $query;
-	if(mysqli_query($con, $query))
-	{
-		$data["message"] = "Data Updated";         
-	}
-echo json_encode($data);
-}		
+// 	$user_id = $_SESSION['user_id'];
+// 	$country_id = $_POST['country'];
+// 	$state_id = $_POST['state'];
+// 	$city_id = $_POST['city'];
+// 	//$area_id = mysqli_real_escape_string($con, $form_data->area_id);
+// 	$address = $_POST['address'];
+// 	$pin_code = $_POST['pincode'];
+// 	//$add_type = mysqli_real_escape_string($con, $form_data->add_type);
+// 	$landmark = $_POST['landmark'];
+	
+// }		
 
 
 if($_REQUEST['action']=='delete')
 {
-	$query = "DELETE from tbl_address  WHERE addr_id='".$form_data->id."'";
+	$query = "DELETE from tbl_address  WHERE addr_id='".$_POST['id']."'";
 	if(mysqli_query($con, $query))
 	{
 		$data["message"] = "Deleted Successfully";         
--- API/user_api.php
+++ API/user_api.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
  //http://localhost:8000/SHREDDERSBAY_API/API/user_api.php?action=select
- session_start();
+session_start();
 include("../DB.php");
 $form_data = json_decode(file_get_contents("php://input"));
 $data = array();
